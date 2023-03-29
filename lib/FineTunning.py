268b153695a0aad0798f17e107bf123262e76e8a
apendindan <apendindan@paho.org>
2023-03-29 14:24:20 -0300

    Initial commit

--- lib/FineTunning.py
+++ lib/FineTunning.py
@@ -1,38 +1,33 @@
 import openai
 import os
-import json
 
-openai.api_key = os.environ["OPENAI_API_KEY"] # configure sua chave de API OpenAI
+openai.api_key = os.environ["OPENAI_API_KEY"]  # configure sua chave de API OpenAI
 
-# Carregando os dados de treinamento do arquivo JSON
-with open('squad_data.json', 'r') as f:
-    data = json.load(f)
 
-# Preparando os dados de treinamento para o formato esperado pela API da OpenAI
-examples = []
-for item in data:
-    context = item['context']
-    for qa in item['qas']:
-        question = qa['question']
-        answer = qa['answer']['text']
-        examples.append({'text': f'{question} {context}', 'label': answer})
+def upload_de_dados():
+    # Os dados tem que estar no formato JSONL, salve como for necessário e depois rode a linha abaixo no terminal
+    # openai tools fine_tunes.prepare_data -f estrategiaexemplos.csv
+    openai.File.create(
+        file=open("estrategiaexemplos.jsonl", "rb"),
+        purpose='fine-tune'
+    )
+    file_id = openai.FineTune.list()[-1]["id"]
+    print(file_id)
+    return file_id
 
-# Treinando o modelo de perguntas e respostas usando o conjunto de dados SQuAD
-model_base = 'text-davinci-003'
-fine_tune_args = {
-    'model': model_base,
-    'train_dataset': examples,
-    'validation_dataset': examples,
-    'epochs': 5,
-    'batch_size': 32,
-    'learning_rate': 1e-5,
-    'max_seq_length': 512,
-    'validation_split': 0.2,
-    'overwrite': True,
-    'model_name': 'Estrategias de Busca',
-    'model_tags': ['Estrategias_de_Busca']
-}
-openai.FineTune.create(**fine_tune_args)
 
-model_id = openai.FineTune.list_models(tag="my_tag")[0]['id']
-print(model_id)
+def primeiro_treino(file_id):
+    # Treinando o modelo de perguntas e respostas.
+    model_base = 'curie'
+    openai.FineTune.create(
+        training_file=file_id,
+        model=model_base
+    )
+
+    # Aqui é para salvar o id do modelo para ser usado dps. A função de entrega o id do ultimo modelo treinado.
+    model_id = openai.FineTune.list()[-1]["id"]
+    print(model_id)
+
+
+dada_id = upload_de_dados()
+primeiro_treino(dada_id)

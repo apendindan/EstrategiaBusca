e49db8ea696907d1e7f542101e37b870b38407a4
apendindan <apendindan@paho.org>
2023-04-05 10:23:00 -0300

    ALterações no chatbot para funcionar com fine-tunning

--- lib/ChatBot.py
+++ lib/ChatBot.py
@@ -1,5 +1,4 @@
 import os
-import json
 import tkinter as tk
 import openai
 
@@ -8,32 +7,35 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def send_message():
     # Recebe questão e imprime
-    user_question = json.loads(json.dumps({"role": "user", "content": user_input.get().strip()}))  # Recebe a questão
-    imprime_fala(user_question)                                 # Imprime a questão na janela
+    global historico
+    user_question = user_input.get().strip()  # Recebe a questão
+    imprime_fala("User",user_question)                          # Imprime a questão na janela
     user_input.delete(0, tk.END)                                # Apaga o que o usuário escreveu da barra de digitacão
     user_input.insert(tk.END, "... Carregando a resposta ...")  # Escreve na barra de digitação
     window.update()                                             # Atualiza a janela para tudo aparecer para o usuário
-    history.append(user_question)                               # Salva a pergunta que o usuário fez no histórico
+    historico += "User:"+user_question+"\n\n"                   # Salva a pergunta que o usuário fez no histórico
 
     # Formula a resposta e imprime
-    response = openai.ChatCompletion.create(  # Faz a resposta utilizando a API da OpenAI
-        model="gpt-3.5-turbo",                # Define o modelo a ser usado. Necessário atualizar pós fine-tunning
-        messages=history                      # O bot recebe tudo do histórico para continuar a conversa
-    )
-    bibi_response = response["choices"][0]["message"]  # Seleciona apenas a fala que o bot adicinou por último
-    history.append(bibi_response)                      # Adiciona essa fala ao histórico
-    imprime_fala(bibi_response)                        # Imprime a fala na janela
-    user_input.delete(0, tk.END)                       # Apaga o "carregando" da barra de digitação
+    response = openai.Completion.create(  # Faz a resposta utilizando a API da OpenAI
+        model="curie",                    # Define o modelo a ser usado. Necessário atualizar pós fine-tunning
+        prompt=historico+"Bibi:",         # O bot recebe tudo do histórico para continuar a conversa
+        max_tokens=200,
+        temperature=0
+     )
+    bibi_response = response["choices"][0]["text"]  # Seleciona apenas a fala que o bot adicinou por último
+    historico += "Bibi:"+bibi_response+"\n\n"       # Adiciona essa fala ao histórico
+    imprime_fala("Bibi",bibi_response)              # Imprime a fala na janela
+    user_input.delete(0, tk.END)                    # Apaga o "carregando" da barra de digitação
 
 
-def imprime_fala(message):
+def imprime_fala(role, message):
     global speaker, color, numb
 
     # Descobre se a fala será do assistant ou user, salva o nome e o numero da tag correspondente
-    if message["role"] == "assistant":
-        speaker = "Bibliotecária: "
+    if role == "Bibi":
+        speaker = "Bibi: "
         numb = 0                        # Esse é o numero da tag, que será colocado na lista Tags
-    elif message["role"] == "user":
+    elif role == "User":
         speaker = "Você: "
         numb = 2                        # Esse é o numero da tag, que será colocado na lista Tags
 
@@ -55,7 +57,7 @@ def imprime_fala(message):
 
     # Imprime as falas e os nomes
     conversation_history.insert(tk.END, speaker, Tags[numb])
-    conversation_history.insert(tk.END, message["content"] + "\n\n", Tags[numb+1])
+    conversation_history.insert(tk.END, message + "\n\n", Tags[numb+1])
 
     # Encerra
     conversation_history.config(state=tk.DISABLED)
@@ -98,31 +100,22 @@ def cria_janela():
 cria_janela()
 
 # Define os settings que vamos mandar para o ChatGPT explicando como funcionar no ChatBot
-settings = json.loads("""
-    {"role": "system", 
-    "content": "Você é a Bibliotecária da BIREME. Apenas responda o que for relacionado a estratégias de busca, e, se 
-    algo fugir do tema, responda \\"Desculpe, não sou capacitada para responder nada além de questões sobre estratégias 
-    de busca\\".\\n\\nExemplo:\\nUsuário:exercicios aerobicos na insuficiencia cardiaca em idosos\\n\\nBibi:Para os 
-    temas abaixo recomendamos usar termos livres em diferentes idiomas; pesquisar os descritores de assunto mais 
-    adequados para cada um.\\nexercicio aeróbico, Aerobic Exercise\\ninsuficiencia cardíaca, Heart Failure\\nExemplo 
-    de busca:\\n(\\"exercicio aeróbico\\" OR \\"Aerobic Exercise\\") AND (\\"insuficiencia cardíaca\\" OR \\"Heart 
-    Failure\\")\\nPara idosos - use o filtro LIMITE disponível na interface de resultado, essa faixa etária está 
-    disponível na opção Mais filtros, clique e inclua para selecionar a faixa etária.\\n    *os termos da busca são 
-    recuperados no título, resumo e descritor de assunto (quando aplicável) dos documentos"}
-""".replace(" \n", " "))  # Esse replace é para que as quebras de linhas sirvam apenas para a leitura do código
+with open(os.getcwd()+'/Texts/settings', 'r') as file:
+    settings = file.read()
+
+with open(os.getcwd()+'/Texts/examples', 'r') as file:
+    examples = file.read()
 
 # Define a mensagem de apresentação do ChatBot
-initial_message = json.loads("""  
-    {"role": "assistant", 
-    "content": "Olá, sou a Bibliotecária virtual da BIREME. Meu objetivo aqui é ajudar você a formular estratégias de 
-    busca para conseguir achar os artigos que precisa! Me diga, o que está querendo buscar?"}
-""".replace(" \n", " "))  # Esse replace é para que as quebras de linhas sirvam apenas para a leitura do código
+with open(os.getcwd()+'/Texts/mensagem_inicial', 'r') as file:
+    initial_message = file.read()
+
 
 # Cria o histórico onde vamos salvar a conversa inteira
-history = json.loads('[]')
-history.append(settings)
-history.append(initial_message)
-imprime_fala(initial_message)
+global historico
+historico = settings + "\n\n\n" + examples + "\n\n\n"  # Caso queira adicionar exemplos pro sistema, tirar a #
+historico += "Bibi:"+initial_message+"\n\n"
+imprime_fala("Bibi", initial_message)
 
 # Começa o loop do chat
 window.mainloop()
